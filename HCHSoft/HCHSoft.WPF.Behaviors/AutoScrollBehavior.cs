2d33afb851cf654b96066960ae2af27179930e8c
hechao <vagrant_hch@hotmail.com>
2016-01-13 15:12:25 +0800

    Add WPF.Behaviors\WPF.Misc proj
    WPF.Behavior:
    1、ListBox add AutoScrollBehavior
    WPF.Misc:
    1、offer methods of getting handle in WPF

---
+++ HCHSoft/HCHSoft.WPF.Behaviors/AutoScrollBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Interactivity;
+
+namespace HCHSoft.WPF.Behaviors
+{
+    public class AutoScrollBehavior : Behavior<ListBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.SelectionChanged += new SelectionChangedEventHandler(AssociatedObject_SelectionChanged);
+        }
+
+        void AssociatedObject_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is ListBox)
+            {
+                var listbox = (sender as ListBox);
+                if (listbox.SelectedItem != null)
+                {
+                    listbox.UpdateLayout();
+                    listbox.ScrollIntoView(listbox.SelectedItem);
+                }
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.SelectionChanged -= new SelectionChangedEventHandler(AssociatedObject_SelectionChanged);
+        }
+    }
+}
---
+++ HCHSoft/HCHSoft.WPF.Misc/InteropHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace HCHSoft.WPF.Misc
+{
+    public class InteropHelper
+    {
+        public static IntPtr GetWindowHnadle(Window win)
+        {
+            return new WindowInteropHelper(win).Handle;
+        }
+
+        public static IntPtr GetVisualHandle(UIElement element)
+        {
+            return ((HwndSource)PresentationSource.FromVisual(element)).Handle;
+        }
+    }
+}
